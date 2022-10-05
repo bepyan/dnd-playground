5bda7f546135e3d0d7e64b948f63126600b3f10a
edward <bepyan@naver.com>
2022-10-05 23:01:15 +0900

    feat. custom DND - drag

--- src/components/DNDExample.tsx
+++ src/components/DNDExample.tsx
@@ -1,5 +1,6 @@
 import { TItems, TItemType } from '@/pages/dnd';
 import { $ } from '@/utils';
+import { useEffect } from 'react';
 
 export default function DNDExample({
   items,
@@ -8,6 +9,52 @@ export default function DNDExample({
   items: TItems;
   setItems: (items: TItems) => void;
 }) {
+  useEffect(() => {
+    document.addEventListener('mousedown', (e) => {
+      const target = e.target as HTMLElement;
+      const item = target?.closest('.dnd-item');
+      if (!item) return;
+
+      const itemRect = item.getBoundingClientRect();
+
+      const ghostItem = item.cloneNode(true) as HTMLElement;
+      ghostItem.style.position = 'fixed';
+      ghostItem.style.top = `${itemRect.top}px`;
+      ghostItem.style.left = `${itemRect.left}px`;
+      ghostItem.style.width = `${itemRect.width}px`;
+      ghostItem.style.height = `${itemRect.height}px`;
+      ghostItem.classList.add('ghost');
+      item.classList.add('placeholder');
+
+      setTimeout(() => {
+        ghostItem.style.opacity = '0.95';
+        ghostItem.style.boxShadow = '0 30px 60px rgba(0, 0, 0, .3)';
+        ghostItem.style.transform = 'scale(1.05)';
+      }, 0);
+
+      document.querySelector('#__next')?.appendChild(ghostItem);
+
+      const moveHandler = (moveEvent: MouseEvent) => {
+        const deltaX = moveEvent.pageX - e.pageX;
+        const deltaY = moveEvent.pageY - e.pageY;
+
+        ghostItem.style.top = `${itemRect.top + deltaY}px`;
+        ghostItem.style.left = `${itemRect.left + deltaX}px`;
+      };
+
+      document.addEventListener('mousemove', moveHandler);
+      document.addEventListener(
+        'mouseup',
+        () => {
+          document.removeEventListener('mousemove', moveHandler);
+          ghostItem.remove();
+          item.classList.remove('placeholder');
+        },
+        { once: true },
+      );
+    });
+  }, []);
+
   return (
     <div className="p-4">
       <div className="mb-2">
@@ -21,21 +68,14 @@ export default function DNDExample({
             <div
               key={key}
               className={$(
-                'flex flex-col gap-3 rounded-xl bg-gray-200 p-4 ring-1 ring-gray-300 transition-shadow dark:bg-[#000000]',
+                'flex flex-col gap-3 rounded-xl bg-gray-200 p-4 ring-1 ring-gray-300 dark:bg-[#000000]',
               )}
             >
               <span className="text-xs font-semibold">{key.toLocaleUpperCase()}</span>
               {items[key as TItemType].map((item, index) => (
-                <div key={item.id} className="cursor-grab">
-                  <div
-                    className={$(
-                      'rounded-lg bg-white p-4 transition-shadow dark:bg-[#121212]',
-                      // snapshot.isDragging ? 'bg-opacity-90 shadow-2xl shadow-gray-400' : 'shadow',
-                    )}
-                  >
-                    <h5 className="font-semibold">{item.title}</h5>
-                    <span className="text-sm text-gray-500">Make the world beatiful</span>
-                  </div>
+                <div key={item.id} className="dnd-item">
+                  <h5 className="font-semibold">{item.title}</h5>
+                  <span className="text-sm text-gray-500">Make the world beatiful</span>
                 </div>
               ))}
             </div>
