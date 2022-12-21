d1d822b0245305697d1e21e640f68054c9cf6a06
edward <bepyan@naver.com>
2022-12-22 00:07:34 +0900

    chore. google search console

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function MyDocument() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="mCzku7m_j1jyzVaRRmn-F_UvjF1E6XinYX6OdJkV7Ts"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
